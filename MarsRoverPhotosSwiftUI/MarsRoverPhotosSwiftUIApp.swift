0fb6931b17b59e7fd0e2419100b25be5a165956e
Alex Baluhtin <aliakseibalukhtsin24@gmail.com>
2024-03-13 21:37:01 +0100

    Initial Commit

---
+++ MarsRoverPhotosSwiftUI/MarsRoverPhotosSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsRoverPhotosSwiftUIApp.swift
+//  MarsRoverPhotosSwiftUI
+//
+//  Created by Alex Balukhtsin on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarsRoverPhotosSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
